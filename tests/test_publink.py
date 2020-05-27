d7f134c39e23065e9a5e47f724b17fdae47e0c99
dwief <dwieferich@usgs.gov>
2020-05-27 04:54:57 -0600

    add test_resolve_doi, remove default test examples

--- tests/test_publink.py
+++ tests/test_publink.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python
 """Tests for `publink` package."""
-import pytest
+
+from publink import publink
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_resolve_doi():
+    """Ensure bad url fails."""
+    bad_doi = 'this_is_a_bad_doi'
+    assert publink.resolve_doi(bad_doi) is False
